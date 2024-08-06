94eb9e3ef82b12798366b7e19430ab404b4972ef
KD <kaitlyn.davis@hashicorp.com>
2024-08-06 12:39:36 -0700

    removed SG creation from module

--- main.tf
+++ main.tf
@@ -13,17 +13,6 @@ data "aws_ami" "amzn-linux-2023-ami" {
   }
 }
 
-# Check if the security group exists
-data "aws_security_group" "existing" {
-  filter {
-    name   = "group-name"
-    values = [var.sg_name_allow_http]
-  }
-
-  # Uncomment the VPC ID if you need to specify the VPC
-  # vpc_id = "vpc-xxxxxxxx"
-}
-
 # check existing instance count for naming tag
 data "aws_instances" "existing_simple_ec2" {
   filter{
@@ -34,34 +23,7 @@ data "aws_instances" "existing_simple_ec2" {
 
 
 locals {
-  sg_exists = length(data.aws_security_group.existing.id)
   simple_ec2_count = length(data.aws_instances.existing_simple_ec2.ids)
-}
-
-# Create Security Group for compute http access
-resource "aws_security_group" "allow-http" {
-  count = local.sg_exists == 0 ? 1 : 0
-  name  = "allow-http"
-}
-
-# Security Group - Ingress Rule
-resource "aws_vpc_security_group_ingress_rule" "allow-http" {
-  count = local.sg_exists == 0 ? 1 : 0
-
-  security_group_id = aws_security_group.allow-http[count.index].id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-
-# Security Group - Egress Rule
-resource "aws_vpc_security_group_egress_rule" "allow-all-traffic" {
-  count = local.sg_exists == 0 ? 1 : 0
-
-  security_group_id = aws_security_group.allow-http[count.index].id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = -1
 }
 
 
